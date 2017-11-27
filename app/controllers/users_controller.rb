63f1d5af592527c21094e23aae6ae275d354db29
olindos <yason4ik87@gmail.com>
2017-11-27 14:44:44 +0200

    add_view_new

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   before_action :set_person, only: [:show, :edit, :destroy, :update]
 
   def index
-    @user = User.order(:user_id)
+    @user = User.all
   end
 
   def new
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     respond_to do |format|
       if @user.save
-        format.html{ redirect_to users_url, notice: 'Пользователь #{@user.nickname} был успешно создан' }
+        format.html{ redirect_to @user, notice: "Пользователь #{@user.nickname} был успешно создан" }
       else
         format.html{ render action: 'new' }
       end
@@ -23,7 +23,7 @@ class UsersController < ApplicationController
   def update
     respond_to do |format|
       if @user.update(user_params)
-        format.html{ redirect_to users_url, notice: 'Данные пользователя #{@user.nickname} успешно обновлены' }
+        format.html{ redirect_to @user, notice: "Данные пользователя #{@user.nickname} успешно обновлены" }
       else
         format.html{ render action: 'edit' }
       end
@@ -39,7 +39,7 @@ class UsersController < ApplicationController
   def destroy
     @user.destroy
     respond_to do |format|
-      format.html{ respond_to users_url, notice: 'Пользователь #{@user.nickname} успешно удален'}
+      format.html{ respond_to users_url, notice: "Пользователь #{@user.nickname} успешно удален" }
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,3 @@
 Rails.application.routes.draw do
-  get 'users/index'
-
-  get 'users/create'
-
-  get 'users/update'
-
-  get 'users/edit'
-
-  get 'users/show'
-
-  get 'users/destroy'
-
-  get 'users/new'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users
 end
