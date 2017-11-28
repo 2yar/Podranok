50ff596318ca8a5901fc3bd2c4890bf5b3fdd5b0
gidpoiiohika <adacer73044210@gmail.com>
2017-11-28 12:02:17 +0200

    correction of errors in products controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,9 +7,7 @@ class ProductsController < ApplicationController
   end
 
   # GET /products/1
-  def show
-
-  end
+  def show end
 
   # GET /products/new
   def new
@@ -17,8 +15,7 @@ class ProductsController < ApplicationController
   end
 
   # GET /products/1/edit
-  def edit
-  end
+  def edit end
 
   # POST /products
   def create
