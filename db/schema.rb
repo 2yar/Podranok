fde09f09e497ae4a07ad5555ab03df9cd6c8e7a2
olindos <yason4ik87@gmail.com>
2017-11-30 15:32:55 +0200

    removed model itenin shema

--- db/schema.rb
+++ db/schema.rb
@@ -26,15 +26,6 @@ ActiveRecord::Schema.define(version: 20171130121448) do
     t.index ["product_id"], name: "index_line_items_on_product_id"
   end
 
-  create_table "line_itens", force: :cascade do |t|
-    t.integer  "product_id"
-    t.integer  "cart_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["cart_id"], name: "index_line_itens_on_cart_id"
-    t.index ["product_id"], name: "index_line_itens_on_product_id"
-  end
-
   create_table "products", force: :cascade do |t|
     t.string   "name"
     t.text     "description"
