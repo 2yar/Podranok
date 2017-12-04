9edeede988e5d126d19a6121c6890ef08f16310c
olindos <yason4ik87@gmail.com>
2017-12-04 15:37:52 +0200

    delete product out line_item

---
+++ app/models/concerns/current_cart.rb
@@ -0,0 +1,12 @@
+module CurrentCart
+  extend Activesupport::Concern
+
+  private
+
+  def set_cart
+    @cart = Cart.find(session[:cart_id])
+  rescue ActiveRecord::RecordNotFound
+    @cart = Cart.create
+    session[:cart_id] = @cart_id
+  end
+end
