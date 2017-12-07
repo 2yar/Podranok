64a41610987568a574561f0c4b811d5a24447022
gidpoiiohika <adacer73044210@gmail.com>
2017-12-07 16:14:18 +0200

    sending electronic emails

---
+++ app/mailers/order_notifier_mailer.rb
@@ -0,0 +1,23 @@
+class OrderNotifierMailer < ApplicationMailer
+  default from: 'Sam Ruby <depot@example.com>'
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.order_notifier_mailer.received.subject
+  #
+  def received(order)
+    @order = order
+    mail to: order.email, subject: 'Подтверждение заказа в Podranok Store'
+  end
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.order_notifier_mailer.shipped.subject
+  #
+  def shipped
+    @order = order
+
+    mail to: order.email, subject: 'Заказ из Podranok Store отправлен'
+  end
+end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,4 +51,17 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "gmail.com",
+    authentication: "plain",
+    user_name: "mygmailusername@gmail.com",
+    password: "myGmailPassword",
+    enable_starttls_auto: true
+  }
 end
