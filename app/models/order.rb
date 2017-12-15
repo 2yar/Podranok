0363e7c7641b49373bea9a5f17390db6ebf245f3
gidpoiiohika <adacer73044210@gmail.com>
2017-12-15 17:06:57 +0200

    fixed bugs

--- app/models/order.rb
+++ app/models/order.rb
@@ -8,7 +8,7 @@ class Order < ApplicationRecord
 
   validates :name, :address, :email, presence: true
   validates :pay_type, inclusion: PAYMENT_TYPES
-  validates :type_of_delivery, inclusion: TYPE_Of_DELIVERY
+  validates :type_of_delivery, inclusion: TYPE_OF_DELIVERY
 
   def add_line_items_from_cart(cart)
     cart.line_items.each do |item|
