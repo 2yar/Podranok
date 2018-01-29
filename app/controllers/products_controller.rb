95502d3c9241fad291e336c858ad95a25afc4eba
gidpoiiohika <adacer73044210@gmail.com>
2018-01-29 20:44:44 +0200

    fixed bugs

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -58,7 +58,6 @@ class ProductsController < ApplicationController
   # Use callbacks to share common setup or constraints between actions.
   def set_product
     @product = Product.find(params[:id])
-
   end
 
   # Never trust parameters from the scary internet, only allow the white list through.
