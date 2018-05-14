d7bbbdf138aafa0f72ac0f3e9473092f6c5d9b39
gidpoiiohika <adacer73044210@gmail.com>
2018-05-14 14:59:08 +0300

    Added page navigation of products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,7 +4,7 @@ class ProductsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show]
 
   def index
-    @products = Product.all
+    @products = Product.paginate(page: params[:page], per_page: 8)
   end
 
   def show; end
