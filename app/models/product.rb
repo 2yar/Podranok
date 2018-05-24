0b1f1b30a9e24ff32504c495cc25b08c57443ef1
gidpoiiohika <adacer73044210@gmail.com>
2018-05-24 14:46:07 +0300

    changed search syntax

--- app/models/product.rb
+++ app/models/product.rb
@@ -10,7 +10,7 @@ class Product < ApplicationRecord
   belongs_to :category
   
   def self.search(search)
-    where("name LIKE ? OR price LIKE ?", "%#{search}%", "%#{search}%")
+    where("name ILIKE ? OR price ILIKE ?", "%#{search}%", "%#{search}%")
   end
 
   private
